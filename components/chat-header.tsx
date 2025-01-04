32e09a26441f38a24f2eb8a76ba7bd1a5daf382d
Vincent <xiaoyaoke8986@gmail.com>
2025-01-04 12:16:34 +0800

    fix: delete 'Deploy with Vercel'

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -1,17 +1,17 @@
-'use client';
+'use client'
 
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import { useWindowSize } from 'usehooks-ts';
+// import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useWindowSize } from 'usehooks-ts'
 
-import { ModelSelector } from '@/components/model-selector';
-import { SidebarToggle } from '@/components/sidebar-toggle';
-import { Button } from '@/components/ui/button';
-import { PlusIcon, VercelIcon } from './icons';
-import { useSidebar } from './ui/sidebar';
-import { memo } from 'react';
-import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
-import { VisibilityType, VisibilitySelector } from './visibility-selector';
+import { ModelSelector } from '@/components/model-selector'
+import { SidebarToggle } from '@/components/sidebar-toggle'
+import { Button } from '@/components/ui/button'
+import { PlusIcon, VercelIcon } from './icons'
+import { useSidebar } from './ui/sidebar'
+import { memo } from 'react'
+import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip'
+import { VisibilityType, VisibilitySelector } from './visibility-selector'
 
 function PureChatHeader({
   chatId,
@@ -19,15 +19,15 @@ function PureChatHeader({
   selectedVisibilityType,
   isReadonly,
 }: {
-  chatId: string;
-  selectedModelId: string;
-  selectedVisibilityType: VisibilityType;
-  isReadonly: boolean;
+  chatId: string
+  selectedModelId: string
+  selectedVisibilityType: VisibilityType
+  isReadonly: boolean
 }) {
-  const router = useRouter();
-  const { open } = useSidebar();
+  const router = useRouter()
+  const { open } = useSidebar()
 
-  const { width: windowWidth } = useWindowSize();
+  const { width: windowWidth } = useWindowSize()
 
   return (
     <header className="flex sticky top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
@@ -40,8 +40,8 @@ function PureChatHeader({
               variant="outline"
               className="order-2 md:order-1 md:px-2 px-2 md:h-fit ml-auto md:ml-0"
               onClick={() => {
-                router.push('/');
-                router.refresh();
+                router.push('/')
+                router.refresh()
               }}
             >
               <PlusIcon />
@@ -52,12 +52,7 @@ function PureChatHeader({
         </Tooltip>
       )}
 
-      {!isReadonly && (
-        <ModelSelector
-          selectedModelId={selectedModelId}
-          className="order-1 md:order-2"
-        />
-      )}
+      {!isReadonly && <ModelSelector selectedModelId={selectedModelId} className="order-1 md:order-2" />}
 
       {!isReadonly && (
         <VisibilitySelector
@@ -67,7 +62,7 @@ function PureChatHeader({
         />
       )}
 
-      <Button
+      {/* <Button
         className="bg-zinc-900 dark:bg-zinc-100 hover:bg-zinc-800 dark:hover:bg-zinc-200 text-zinc-50 dark:text-zinc-900 hidden md:flex py-1.5 px-2 h-fit md:h-[34px] order-4 md:ml-auto"
         asChild
       >
@@ -78,11 +73,11 @@ function PureChatHeader({
           <VercelIcon size={16} />
           Deploy with Vercel
         </Link>
-      </Button>
+      </Button> */}
     </header>
-  );
+  )
 }
 
 export const ChatHeader = memo(PureChatHeader, (prevProps, nextProps) => {
-  return prevProps.selectedModelId === nextProps.selectedModelId;
-});
+  return prevProps.selectedModelId === nextProps.selectedModelId
+})
